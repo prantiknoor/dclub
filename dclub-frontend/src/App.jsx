a0d5b06088acabef8c8568a947bb57ee81492dc1
Prantik <coderprantik@gmai.com>
2022-03-18 14:17:55 +0600

    easier verison

--- dclub-frontend/src/App.jsx
+++ dclub-frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import ProposalCard from './components/ProposalCard'
-import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <div className="app-body">
-        <Dashboard></Dashboard>
+        <ProposalCard></ProposalCard>
       </div>
     </div>
   );
